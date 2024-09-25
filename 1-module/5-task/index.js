d1ef7ec67b80bb8782374ccc7a985abc884d1e12
DvornikovAS <DvornikovAS@admlr.lipetsk.ru>
2024-09-25 16:00:56 +0300

    refactor: 5th task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,9 +1,3 @@
 function truncate(str, maxlength) {
-  if (str.length >= maxlength) {
-    newStr = str.slice(0, maxlength - 1);
-    return `${newStr}…`;
-  } else {
-    return str;
-  }
+  return str.length >= maxlength ? `${str.slice(0, maxlength - 1)}…` : str;
 }
-
