a27e28da001cf1828743b1bc1b894894314e06f5
kxxdree <truebackward@gmail.com>
2024-10-09 19:56:03 +0300

    feat: add 4th module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  for (let i = 0; i < friends.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = friends[i].firstName + " " + friends[i].lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
