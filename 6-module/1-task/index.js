707cb8aed63a853c3c11b42aa6b4a633d0385bc7
kxxdree <truebackward@gmail.com>
2024-10-20 14:44:39 +0300

    feat: add 6th module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,47 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement("table");
+    this.createHeader();
+    this.createBody(rows);
+  }
+
+  createHeader() {
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
+    let headers = ["Имя", "Возраст", "Зарплата", "Город"];
+
+    headers.forEach((headerTitle) => {
+      let th = document.createElement("th");
+      th.textContent = headerTitle;
+      tr.appendChild(th);
+    });
+
+    thead.appendChild(tr);
+    this.elem.appendChild(thead);
+  }
+
+  createBody(rows) {
+    let tbody = document.createElement("tbody");
+
+    rows.forEach((row) => {
+      let tr = document.createElement("tr");
+
+      Object.values(row).forEach((text) => {
+        let td = document.createElement("td");
+        td.textContent = text;
+        tr.appendChild(td);
+      });
+
+      let td = document.createElement("td");
+      let button = document.createElement("button");
+
+      button.textContent = "[X]";
+      button.addEventListener("click", () => tr.remove());
+      td.appendChild(button);
+      tr.appendChild(td);
+      tbody.appendChild(tr);
+    });
+
+    this.elem.appendChild(tbody);
   }
 }
