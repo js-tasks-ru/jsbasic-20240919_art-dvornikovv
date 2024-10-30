8cc1ba7d172a739459728eaefb874af87709f620
DvornikovAS <DvornikovAS@admlr.lipetsk.ru>
2024-10-30 11:20:38 +0300

    feat: add 2nd task of 8th module

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,43 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+        </div>
+      </div>
+    `);
+
+    this.render();
+  }
+
+  render() {
+    const inner = this.elem.querySelector(".products-grid__inner");
+    inner.innerHTML = "";
+
+    const filteredProducts = this.products.filter((product) => {
+      return (
+        (!this.filters.noNuts || !product.nuts) &&
+        (!this.filters.vegeterianOnly || product.vegeterian) &&
+        (!this.filters.maxSpiciness ||
+          product.spiciness <= this.filters.maxSpiciness) &&
+        (!this.filters.category || product.category === this.filters.category)
+      );
+    });
+
+    filteredProducts.forEach((product) => {
+      const card = new ProductCard(product);
+      inner.append(card.elem);
+    });
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.render();
   }
 }
